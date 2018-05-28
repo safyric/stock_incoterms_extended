a97a258fb6da0df2e941ee2d5ec1af9055e37714
jchanme <39323414+jchanme@users.noreply.github.com>
2018-05-28 17:17:41 +0800

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -9,6 +9,7 @@
     'version': '0.1',
     'depends': ['stock', 'product'],
     'data': [
+        'security/ir.model.access.csv',
         'data/stock_delivery_points_data.xml',
         'views/stock_delivery_points_views.xml',
     ],
