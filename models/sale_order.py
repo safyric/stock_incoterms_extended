e10db0a83c273c0fb73a8d2c334d6b871d789581
jchanme <39323414+jchanme@users.noreply.github.com>
2020-03-18 20:53:08 +0800

    Update sale_order.py

--- models/sale_order.py
+++ models/sale_order.py
@@ -5,4 +5,6 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    delivery_place = fields.Many2one('stock.delivery.place', 'Incoterms Port', 'Incoterms Extended: ports')
+    delivery_place = fields.Many2one(
+        'stock.delivery.place', 'Incoterms Port',
+        help="Incoterms port")
